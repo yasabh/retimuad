5ffeac0c710eda99a8d1c81bfc738391f53b03a4
enzeeeh <enzi.muzakki22@gmail.com>
2024-11-20 18:35:30 +0100

    update config kafka topic to check the broker first before create topics

--- configs/create_kafka_topics.sh
+++ configs/create_kafka_topics.sh
@@ -1,18 +1,28 @@
 #!/bin/bash
 
-KAFKA_BROKER="kafka:9092"  # Use the internal Kafka network address within Docker
+KAFKA_BROKER="kafka:9092"
+ZOOKEEPER_HOST="zookeeper:2181"
 TOPICS=("images" "logs" "audio")
 
+# Check if any broker is registered with Zookeeper
+BROKER_COUNT=$(echo "ls /brokers/ids" | zkCli.sh -server $ZOOKEEPER_HOST 2>/dev/null | grep -o '[0-9]' | wc -l)
+
+if [[ "$BROKER_COUNT" -eq 0 ]]; then
+    echo "No brokers are connected to Zookeeper. Exiting..."
+    exit 1
+else
+    echo "$BROKER_COUNT broker(s) connected to Zookeeper."
+fi
+
+# Proceed to create topics if broker(s) exist
 for TOPIC in "${TOPICS[@]}"; do
-    # Check if the topic already exists
-    if kafka-topics --bootstrap-server $KAFKA_BROKER --list | grep -wq $TOPIC; then
+    if /opt/bitnami/kafka/bin/kafka-topics.sh --bootstrap-server $KAFKA_BROKER --list | grep -wq $TOPIC; then
         echo "Topic '$TOPIC' already exists."
     else
-        # Attempt to create the topic
-        if kafka-topics --create --bootstrap-server $KAFKA_BROKER --replication-factor 1 --partitions 1 --topic $TOPIC; then
+        if /opt/bitnami/kafka/bin/kafka-topics.sh --create --bootstrap-server $KAFKA_BROKER --replication-factor 1 --partitions 1 --topic $TOPIC; then
             echo "Topic '$TOPIC' created successfully."
         else
-            echo "Error: Failed to create topic '$TOPIC'. Please check Kafka broker status and configuration."
+            echo "Error creating topic '$TOPIC'."
         fi
     fi
 done
